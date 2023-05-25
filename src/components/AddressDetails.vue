eeb55fdc9bd87631925413e2c15ce991320992d4
Janik Hagen <janikhagen@hotmail.de>
2023-05-25 16:08:34 +0200

    Erweitert um die Addressanzeige

---
+++ src/components/AddressDetails.vue
@@ -0,0 +1,15 @@
+<template>
+    {{ address.street +" "+ address.number }} //displays streetname and number
+</template>
+
+<script>
+export default {
+  props: {
+    address: {}
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/components/UserDetails.vue
+++ src/components/UserDetails.vue
@@ -2,22 +2,37 @@
   <div>
     <p @click="loadAddress(user.id)">{{ user }}</p>
   </div>
+  <div v-if = "addresses.length > 0">
+    <div v-for = "address in addresses" :key="address.id">
+      <AddressDetails :address="address">
+      </AddressDetails>
+    </div>
+  </div>
+
 </template>
 
 <script>
 
 import axios from 'axios'
+import AddressDetails from './AddressDetails.vue';
 
 export default {
-  props: {
-    user: {}
-  },
-  methods: {
-    async loadAddress(userId) {
-      var addresses = await axios.get('http://localhost:3000/addresses?userId=' + userId);
-      addresses.data.forEach( address => console.log(address.street + ' ' + address.number))
-    }
-  },
+    props: {
+        user: {}
+    },
+    data() {
+        return {
+            addresses: []
+        };
+    },
+    methods: {
+        async loadAddress(userId) {
+            var addresses = await axios.get("http://localhost:3000/addresses?userId=" + userId);
+            addresses.data.forEach(address => console.log(address.street + " " + address.number));
+            this.addresses = addresses.data; //legt die Daten der Addressen in addresses ab
+        }
+    },
+    components: { AddressDetails }
 }
 </script>
 
